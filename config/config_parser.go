a3f6359e55855d6966cab3abb6a264872ffa829c
Franz Schmidt <kinnarr@hollywuuud.de>
2018-04-09 18:16:46 +0200

    Log which file has toml parser error

--- config/config_parser.go
+++ config/config_parser.go
@@ -15,38 +15,28 @@
 package config
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/felixangell/toml"
+	"go.uber.org/zap"
 )
 
 var FsmConfigRootDir string
 
 func ParseConfig() bool {
-	fileList := []string{}
+	returnValue := true
+
 	_ = filepath.Walk(FsmConfigRootDir, func(path string, f os.FileInfo, err error) error {
 		if strings.HasSuffix(path, ".toml") {
-			fileList = append(fileList, path)
+			if _, err := toml.DecodeFile(path, &MainConfig); err != nil {
+				zap.S().Errorf("Error parsing toml file %s: %s", path, err)
+				returnValue = false
+			}
 		}
 		return nil
 	})
 
-	var tomlBuffer bytes.Buffer
-	for _, file := range fileList {
-		if readedBytes, err := ioutil.ReadFile(file); err == nil {
-			tomlBuffer.Write(readedBytes)
-		}
-	}
-
-	if _, err := toml.Decode(tomlBuffer.String(), &MainConfig); err != nil {
-		fmt.Println(err)
-		return false
-	}
-
-	return true
+	return returnValue
 }
