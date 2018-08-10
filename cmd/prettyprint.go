a922508c81c877ffa2ed2d4995e10a8b4a318482
Franz Schmidt <kinnarr@hollywuuud.de>
2018-08-10 14:59:32 +0200

    Fixes renamed function

--- cmd/prettyprint.go
+++ cmd/prettyprint.go
@@ -49,7 +49,7 @@ var prettyprintCmd = &cobra.Command{
 		if config.Optimize {
 			optimization.OptimizeConfig()
 
-			if !validation.ValidateStates() || !validation.ValidateOutputs() {
+			if !validation.ValidateStates() || !validation.ValidateDefaults() {
 				zap.S().Errorf("Validation failed! See errors above!\n")
 				return
 			}
